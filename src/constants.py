f3b8228da1d9a97d1d99e2a410bfe1d706b63aea
PilotAlex <66020831+AlexandreAero@users.noreply.github.com>
2023-10-17 10:41:04 +0200

    Fixed indent in ``constants.py``

--- src/constants.py
+++ src/constants.py
@@ -1,21 +1,21 @@
-FPS           = 60
-CELL_SIZE     = 5
-SCREEN_SIZE   = (800, 800)
-CLEAR_COLOR   = (0, 0, 0)
+FPS = 60
+CELL_SIZE = 5
+SCREEN_SIZE = (800, 800)
+CLEAR_COLOR = (0, 0, 0)
 MATERIAL_FILE = './src/materials.json'
 
 BRUSH_SMALL = 'brush_small'
-BRUSH_BIG   = 'brush_big'
+BRUSH_BIG = 'brush_big'
 
-MATERIAL_NAME_NONE     = 'none'
-MATERIAL_NAME_SAND     = 'sand'
-MATERIAL_NAME_WATER    = 'water'
-MATERIAL_NAME_LAVA     = 'lava'
-MATERIAL_NAME_ACID     = 'acid'
+MATERIAL_NAME_NONE = 'none'
+MATERIAL_NAME_SAND = 'sand'
+MATERIAL_NAME_WATER = 'water'
+MATERIAL_NAME_LAVA = 'lava'
+MATERIAL_NAME_ACID = 'acid'
 MATERIAL_NAME_TOXICGAS = 'toxicgas'
 
 # Materials can have three different types as of now.
-MATERIAL_TYPE_NONE   = 'none' # Used for empty cells/particles
-MATERIAL_TYPE_SOLID  = 'solid'
+MATERIAL_TYPE_NONE = 'none' # Used for empty cells/particles
+MATERIAL_TYPE_SOLID = 'solid'
 MATERIAL_TYPE_LIQUID = 'liquid'
-MATERIAL_TYPE_GAS    = 'gas'
+MATERIAL_TYPE_GAS = 'gas'
